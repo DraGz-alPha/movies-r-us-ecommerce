2fe4f4cb01612c87611dd27aef7720ca5b0f5ab8
Dustin Martens <dmartens.it@gmail.com>
2020-03-27 22:20:31 -0500

    Created Customer model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_031842) do
+ActiveRecord::Schema.define(version: 2020_03_28_032006) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "address"
+    t.integer "province_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["province_id"], name: "index_customers_on_province_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "imdb_id"
@@ -34,4 +44,5 @@ ActiveRecord::Schema.define(version: 2020_03_28_031842) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "customers", "provinces"
 end
