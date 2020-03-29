2c0ead15f5b78c138456f66ef91e728afc77b3ce
Dustin Martens <dmartens.it@gmail.com>
2020-03-28 20:10:29 -0500

    Added validations for Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :movies, through: :movie_orders
+
+  validates :reference_number, uniqueness: true
+  validates :reference_number, :pst_rate, :gst_rate, :subtotal,
+            :total, :address, presence: true
 end
