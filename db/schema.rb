c886793371ac137ba90b105f4f2f87d2468b54ed
Dustin Martens <dmartens.it@gmail.com>
2020-03-28 19:43:33 -0500

    Created Page model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_004026) do
+ActiveRecord::Schema.define(version: 2020_03_29_004315) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -82,6 +82,14 @@ ActiveRecord::Schema.define(version: 2020_03_29_004026) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "producers", force: :cascade do |t|
