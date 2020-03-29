a6deb57c0aabe746f9c5d63a19d06a874a72a4e8
Dustin Martens <dmartens.it@gmail.com>
2020-03-28 20:01:16 -0500

    Added validations for Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,7 @@ class Movie < ApplicationRecord
   has_many :orders, through: :movie_orders
   has_many :genres, through: :movie_genres
   has_many :producers, through: :movie_producers
+
+  validates :imdb_id, uniqueness: true
+  validates :imdb_id, :title, :description, :price, presence: true
 end
