2d7ee1aa38194eab2cacf1eca22b2ffa8c98127e
Dustin Martens <dmartens.it@gmail.com>
2020-03-28 19:54:46 -0500

    Set up many-to-many relationships for required models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :orders, through: :movie_orders
+  has_many :genres, through: :movie_genres
+  has_many :producers, through: :movie_producers
 end
--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,2 +1,3 @@
 class Producer < ApplicationRecord
+  has_many :movies, through: :movie_producers
 end
