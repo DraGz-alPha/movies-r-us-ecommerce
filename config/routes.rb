6eac7805b4d7dd12692ef39197eab2f4058a4f11
Dustin Martens <dmartens.it@gmail.com>
2020-04-07 21:47:40 -0500

    Added basic shopping cart functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  post 'movies/add_to_cart/:id', to: 'movies#add_to_cart', as: 'add_to_cart'
+  delete 'movies/remove_from_cart/:id', to: 'movies#remove_from_cart', as: 'remove_from_cart'
+
   root to: 'movies#index'
-  get 'movies/show'
+  resources 'movies', only: %i[index show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
