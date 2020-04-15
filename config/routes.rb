e751bc2d038544ef9e0a85977ebb427972c4b528
Dustin Martens <dmartens.it@gmail.com>
2020-04-14 20:07:25 -0500

    Added cancel and success files for checkout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
 
   get 'pages/show'
   post 'movies/add_to_cart/:id', to: 'movies#add_to_cart', as: 'add_to_cart'
+  post 'movies/increment_quantity/:id', to: 'movies#increment_quantity', as: 'increment_quantity'
   delete 'movies/remove_from_cart/:id', to: 'movies#remove_from_cart', as: 'remove_from_cart'
+
+  scope 'checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+  end
 
   root to: 'movies#index'
   resources 'movies', only: %i[index show]
